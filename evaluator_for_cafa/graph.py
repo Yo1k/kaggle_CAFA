5573c249bca93a94964bba890c3d1ddd4d281ba8
Julia <zaikunets@yandex.ru>
2023-08-08 19:30:00 +0300

    правки по ревью

--- evaluator_for_cafa/graph.py
+++ evaluator_for_cafa/graph.py
@@ -6,7 +6,8 @@ import logging
 class Graph:
     """
     Ontology class. One ontology == one namespace
-    DAG is the adjacence matrix (sparse) which represent a Directed Acyclic Graph where
+    DAG is the adjacence matrix (sparse) which represent a Directed Acyclic
+    Graph where
     DAG(i,j) == 1 means that the go term i is_a (or is part_of) j
     Parents that are in a different namespace are discarded
     """
@@ -15,9 +16,12 @@ class Graph:
         terms_dict = {term: {name: , namespace: , def: , alt_id: , rel:}}
         """
         self.namespace = namespace
-        self.dag = []  # [[], ...] terms (rows, axis 0) x parents (columns, axis 1)
-        self.terms_dict = {}  # {term: {index: , name: , namespace: , def: }  used to assign term indexes in the gt
-        self.terms_list = []  # [{id: term, name:, namespace: , def:, adg: [], children: []}, ...]
+        self.dag = []  # [[], ...] terms (rows, axis 0) x parents (columns,
+        # axis 1)
+        self.terms_dict = {}  # {term: {index: , name: , namespace: , def: }
+        # used to assign term indexes in the gt
+        self.terms_list = []  # [{id: term, name:, namespace: , def:, adg: [],
+        # children: []}, ...]
         self.idxs = None  # Number of terms
         self.order = None
         self.toi = None
@@ -26,10 +30,23 @@ class Graph:
 
         rel_list = []
         for self.idxs, (term_id, term) in enumerate(terms_dict.items()):
-            rel_list.extend([[term_id, rel, term['namespace']] for rel in term['rel']])
-            self.terms_list.append({'id': term_id, 'name': term['name'], 'namespace': namespace, 'def': term['def'],
-                                 'adj': [], 'children': []})
-            self.terms_dict[term_id] = {'index': self.idxs, 'name': term['name'], 'namespace': namespace, 'def': term['def']}
+            rel_list.extend(
+                [[term_id, rel, term['namespace']] for rel in term['rel']]
+            )
+            self.terms_list.append({
+                'id': term_id,
+                'name': term['name'],
+                'namespace': namespace,
+                'def': term['def'],
+                'adj': [],
+                'children': []
+            })
+            self.terms_dict[term_id] = {
+                'index': self.idxs,
+                'name': term['name'],
+                'namespace': namespace,
+                'def': term['def']
+            }
             for a_id in term['alt_id']:
                 self.terms_dict[a_id] = copy.copy(self.terms_dict[term_id])
         self.idxs += 1
@@ -46,16 +63,20 @@ class Graph:
                 self.terms_list[j]['children'].append(i)
                 logging.debug("i,j {},{} {},{}".format(i, j, id1, id2))
             else:
-                logging.debug('Skipping branch to external namespace: {}'.format(id2))
+                logging.debug(
+                    'Skipping branch to external namespace: {}'.format(id2)
+                )
         logging.debug("dag {}".format(self.dag))
         # Topological sorting
         self.top_sort()
         logging.debug("order sorted {}".format(self.order))
 
         if orphans:
-            self.toi = np.arange(self.dag.shape[0])  # All terms, also those without parents
+            self.toi = np.arange(self.dag.shape[0])  # All terms, also those
+            # without parents
         else:
-            self.toi = np.nonzero(self.dag.sum(axis=1) > 0)[0]  # Only terms with parents
+            self.toi = np.nonzero(self.dag.sum(axis=1) > 0)[0]  # Only terms
+            # with parents
         logging.debug("toi {}".format(self.toi))
 
         if ia_dict is not None:
@@ -65,7 +86,8 @@ class Graph:
 
     def top_sort(self):
         """
-        Takes a sparse matrix representing a DAG and returns an array with nodes indexes in topological order
+        Takes a sparse matrix representing a DAG and returns an array with
+        nodes indexes in topological order
         https://en.wikipedia.org/wiki/Topological_sorting
         """
         indexes = []
@@ -80,7 +102,8 @@ class Graph:
         queue = np.nonzero(in_degree == 0)[0].tolist()
         # logging.debug("queue {}".format(queue))
 
-        # for each element of the queue increment visits, add them to the list of ordered nodes
+        # for each element of the queue increment visits, add them to the list
+        # of ordered nodes
         # and decrease the in-degree of the neighbor nodes
         # and add them to the queue if they reach in-degree == 0
         while queue:
@@ -95,18 +118,23 @@ class Graph:
                     if in_degree[j] == 0:
                         queue.append(j)
 
-        # if visited is equal to the number of nodes in the graph then the sorting is complete
+        # if visited is equal to the number of nodes in the graph then the
+        # sorting is complete
         # otherwise the graph can't be sorted with topological order
         if visited == rows:
             self.order = indexes
         else:
-            raise Exception("The sparse matrix doesn't represent an acyclic graph")
+            raise Exception(
+                "The sparse matrix doesn't represent an acyclic graph"
+            )
 
     def set_ia(self, ia_dict):
         self.ia = np.zeros(self.idxs, dtype='float')
         for term_id in self.terms_dict:
             if ia_dict.get(term_id):
-                self.ia[self.terms_dict[term_id]['index']] = ia_dict.get(term_id)
+                self.ia[self.terms_dict[term_id]['index']] = ia_dict.get(
+                    term_id
+                )
             else:
                 logging.debug('Missing IA for term: {}'.format(term_id))
         # Convert inf to zero
@@ -116,7 +144,8 @@ class Graph:
 
 class Prediction:
     """
-    The score matrix contains the scores given by the predictor for every node of the ontology
+    The score matrix contains the scores given by the predictor for every node
+    of the ontology
     """
     def __init__(self, ids, matrix, idx, namespace=None):
         self.ids = ids
@@ -126,7 +155,10 @@ class Prediction:
         self.namespace = namespace
 
     def __str__(self):
-        return "\n".join(["{}\t{}\t{}".format(index, self.matrix[index], self.namespace) for index, _id in enumerate(self.ids)])
+        return "\n".join(
+            ["{}\t{}\t{}".format(index, self.matrix[index], self.namespace)
+             for index, _id in enumerate(self.ids)]
+        )
 
 
 class GroundTruth:
@@ -138,7 +170,8 @@ class GroundTruth:
 
 def propagate(matrix, ont, order, mode='max'):
     """
-    Update inplace the score matrix (proteins x terms) up to the root taking the max between children and parents
+    Update inplace the score matrix (proteins x terms) up to the root taking
+    the max between children and parents
     """
     if matrix.shape[0] == 0:
         raise Exception("Empty matrix")
@@ -165,7 +198,3 @@ def propagate(matrix, ont, order, mode='max'):
                     idx = np.ix_(rows, cols)
                     matrix[rows, i] = matrix[idx].max(axis=1)
     return
-
-
-
-
--- evaluator_for_cafa/parser.py
+++ evaluator_for_cafa/parser.py
@@ -25,12 +25,17 @@ def obo_parser(obo_file, valid_rel=("is_a", "part_of")):
                 v = ": ".join(line[1:])
                 if k == "id":
                     # Populate the dictionary with the previous entry
-                    if term_id is not None and obsolete is False and namespace is not None:
-                        term_dict.setdefault(namespace, {})[term_id] = {'name': name,
-                                                                       'namespace': namespace,
-                                                                       'def': term_def,
-                                                                       'alt_id': alt_id,
-                                                                       'rel': rel}
+                    if (
+                        term_id is not None and obsolete is False and
+                        namespace is not None
+                    ):
+                        term_dict.setdefault(namespace, {})[term_id] = {
+                            'name': name,
+                            'namespace': namespace,
+                            'def': term_def,
+                            'alt_id': alt_id,
+                            'rel': rel
+                        }
                     # Assign current term ID
                     term_id = v
 
@@ -53,17 +58,22 @@ def obo_parser(obo_file, valid_rel=("is_a", "part_of")):
                 elif k == "is_a" and k in valid_rel:
                     s = v.split('!')[0].strip()
                     rel.append(s)
-                elif k == "relationship" and v.startswith("part_of") and "part_of" in valid_rel:
+                elif (
+                    k == "relationship" and v.startswith("part_of") and
+                    "part_of" in valid_rel
+                ):
                     s = v.split()[1].strip()
                     rel.append(s)
 
         # Last record
         if obsolete is False and namespace is not None:
-            term_dict.setdefault(namespace, {})[term_id] = {'name': name,
-                                                          'namespace': namespace,
-                                                          'def': term_def,
-                                                          'alt_id': alt_id,
-                                                          'rel': rel}
+            term_dict.setdefault(namespace, {})[term_id] = {
+                'name': name,
+                'namespace': namespace,
+                'def': term_def,
+                'alt_id': alt_id,
+                'rel': rel
+            }
     return term_dict
 
 
@@ -79,13 +89,17 @@ def gt_parser(gt_file, ontologies):
                 p_id, term_id = line[:2]
                 for ont in ontologies:
                     if term_id in ont.terms_dict:
-                        gt_dict.setdefault(ont.namespace, {}).setdefault(p_id, []).append(term_id)
+                        gt_dict.setdefault(ont.namespace, {}).setdefault(
+                            p_id, []
+                        ).append(term_id)
                         break
 
     gts = {}
     for ont in ontologies:
         if gt_dict.get(ont.namespace):
-            matrix = np.zeros((len(gt_dict[ont.namespace]), ont.idxs), dtype='bool')
+            matrix = np.zeros(
+                (len(gt_dict[ont.namespace]), ont.idxs), dtype='bool'
+            )
             ids = {}
             for i, p_id in enumerate(gt_dict[ont.namespace]):
                 ids[p_id] = i
@@ -93,18 +107,25 @@ def gt_parser(gt_file, ontologies):
                     matrix[i, ont.terms_dict[term_id]['index']] = 1
             logging.debug("gt matrix {} {} ".format(ont.namespace, matrix))
             propagate(matrix, ont, ont.order, mode='max')
-            logging.debug("gt matrix propagated {} {} ".format(ont.namespace, matrix))
+            logging.debug(
+                "gt matrix propagated {} {} ".format(ont.namespace, matrix)
+            )
             gts[ont.namespace] = GroundTruth(ids, matrix, ont.namespace)
-            logging.info('Ground truth: {}, proteins {}'.format(ont.namespace, len(ids)))
+            logging.info(
+                'Ground truth: {}, proteins {}'.format(ont.namespace, len(ids))
+            )
 
     return gts
 
 
 def pred_parser(pred_file, ontologies, gts, prop_mode, max_terms=None):
     """
-    Parse a prediction file and returns a list of prediction objects, one for each namespace.
-    If a predicted is predicted multiple times for the same target, it stores the max.
-    This is the slow step if the input file is huge, ca. 1 minute for 5GB input on SSD disk.
+    Parse a prediction file and returns a list of prediction objects,
+    one for each namespace.
+    If a predicted is predicted multiple times for the same target, it stores
+    the max.
+    This is the slow step if the input file is huge, ca. 1 minute for 5GB
+    input on SSD disk.
 
     """
     ids = {}
@@ -125,7 +146,10 @@ def pred_parser(pred_file, ontologies, gts, prop_mode, max_terms=None):
                 ns = ns_dict.get(term_id)
                 if ns in gts and p_id in gts[ns].ids:
                     i = gts[ns].ids[p_id]
-                    if max_terms is None or np.count_nonzero(matrix[ns][i]) <= max_terms:
+                    if (
+                        max_terms is None or
+                        np.count_nonzero(matrix[ns][i]) <= max_terms
+                    ):
                         j = onts[ns].terms_dict.get(term_id)['index']
                         ids[ns][p_id] = i
                         matrix[ns][i, j] = max(matrix[ns][i, j], float(prob))
@@ -137,12 +161,18 @@ def pred_parser(pred_file, ontologies, gts, prop_mode, max_terms=None):
             propagate(matrix[ns], onts[ns], onts[ns].order, mode=prop_mode)
             logging.debug("pred matrix {} {} ".format(ns, matrix))
 
-            predictions.append(Prediction(ids[ns], matrix[ns], len(ids[ns]), ns))
-            logging.info("Prediction: {}, {}, proteins {}".format(pred_file, ns, len(ids[ns])))
+            predictions.append(
+                Prediction(ids[ns], matrix[ns], len(ids[ns]), ns)
+            )
+            logging.info("Prediction: {}, {}, proteins {}".format(
+                pred_file, ns, len(ids[ns]))
+            )
 
     if not predictions:
         # raise Exception("Empty prediction, check format")
-        logging.warning("Empty prediction! Check format or overlap with ground truth")
+        logging.warning(
+            "Empty prediction! Check format or overlap with ground truth"
+        )
 
     return predictions
 
@@ -173,10 +203,12 @@ def ia_parser(file):
 #                 if event == 'end':
 #                     if elem.tag[nsl:] == 'entry':
 #                         acc = elem.find('uniprot:accession', namespaces).text
-#                         for el in elem.iterfind('uniprot:dbReference', namespaces):
+#                         for el in elem.iterfind(
+# 'uniprot:dbReference', namespaces):
 #                             if el.attrib['type'] == 'GO':
 #                                 for at in el.iter():
 #                                     if at.attrib['type'] == 'evidence':
-#                                         fout.write('{}\t{}\t{}\n'.format(acc, el.attrib['id'], at.attrib['value']))
+#                                         fout.write(
+# '{}\t{}\t{}\n'.format(acc, el.attrib['id'], at.attrib['value']))
 #                         elem.clear()
 #     return
